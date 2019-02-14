26c0771deab58439b5ec19b49984749c94008f5d
Bashar Said <bashar.said@vshn.ch>
2019-02-14 18:19:00 +0100

    Refactor command for better test isolation
    Add test for exception being raised

--- django_probes/management/commands/wait_for_database.py
+++ django_probes/management/commands/wait_for_database.py
@@ -5,10 +5,47 @@ from time import sleep, time
 from django.core.management.base import BaseCommand
 from django.db import connection
 from django.db.utils import OperationalError
+
 try:
     TimeoutError
 except NameError:  # Python 2.7
-    TimeoutError = SystemExit  # noqa, pylint: disable=redefined-builtin
+    TimeoutError = RuntimeError  # noqa, pylint: disable=redefined-builtin
+
+
+def wait_for_database():
+    """
+    The main loop waiting for the database connection to come up.
+    """
+    wait_for_db_seconds = 1
+    stable_for_seconds = 4
+    timeout_seconds = 3
+
+    conn_alive_start = None
+    start = time()
+
+    while time() - start < stable_for_seconds:
+
+        # loop until we have a database connection or we run into a timeout
+        while True:
+            elapsed_time = int(time() - start)
+
+            if elapsed_time >= timeout_seconds:
+                raise TimeoutError(
+                    'Could not establish database connection.')
+            try:
+                connection.cursor().execute('SELECT')
+                if not conn_alive_start:
+                    conn_alive_start = time()
+                break
+            except OperationalError as err:
+                err_message = str(err).strip()
+                print('Waiting for database (cause: {msg}) ... {elapsed}s'.
+                      format(msg=err_message, elapsed=elapsed_time))
+                sleep(wait_for_db_seconds)
+
+        print('Connection alive for > {uptime}s'.format(
+            uptime=int(time() - conn_alive_start)))
+        sleep(1)
 
 
 class Command(BaseCommand):
@@ -16,39 +53,17 @@ class Command(BaseCommand):
     A readiness probe you can use for Kubernetes.
 
     If the default database is ready, i.e. willing to accept connections
-    and handling requests, then this call will succeed silently. Otherwise
-    it will throw an exception.
+    and handling requests, then this call will exit successfully. Otherwise
+    the command exits with an error status after reaching a timeout.
     """
     help = 'Probes for database availability'
 
     def handle(self, *args, **options):
-        """Throw an exception if database is not (yet) available."""
-        wait_for_db_seconds = 1
-        stable_for_seconds = 4
-        timeout_seconds = 3
-
-        conn_alive_start = None
-        start = time()
-
-        while time() - start < stable_for_seconds:
-
-            # loop until we have a database connection or we run into a timeout
-            while True:
-                elapsed_time = int(time() - start)
-                if elapsed_time >= timeout_seconds:
-                    raise TimeoutError(
-                        'Could not establish database connection.')
-                try:
-                    connection.cursor().execute('SELECT')
-                    if not conn_alive_start:
-                        conn_alive_start = time()
-                    break
-                except OperationalError as err:
-                    err_message = str(err).strip()
-                    print('Waiting for database (cause: {msg}) ... {elapsed}s'.
-                          format(msg=err_message, elapsed=elapsed_time))
-                    sleep(wait_for_db_seconds)
-
-            print('Connection alive for > {uptime}s'.format(
-                uptime=int(time() - conn_alive_start)))
-            sleep(1)
+        """
+        Wait for a database connection to come up. Exit with error
+        status when a timeout threshold is surpassed.
+        """
+        try:
+            wait_for_database()
+        except TimeoutError as err:
+            raise SystemExit(err)
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,24 +1,30 @@
 """
 Verify that ``python manage.py wait_for_database`` works fine.
 """
-# import pytest
+import pytest
 
-from django.core.management import call_command
+# from django.core.management import call_command
+from django.db.utils import OperationalError
 
 try:
     from unittest.mock import patch
 except ImportError:  # Python 2.7
     from mock import patch
+    TimeoutError = RuntimeError  # noqa, pylint: disable=redefined-builtin
+
+from django_probes.management.commands.wait_for_database \
+    import wait_for_database
 
 
-# @pytest.mark.django_db
-# @patch('django.db.utils.OperationalError')
-# def test_exception_caught_when_connection_absent(mock_db_exception):
-#     """
-#     When database connection is absent related errors are caught.
-#     """
-#     call_command('wait_for_database')
-#     assert mock_db_exception.called
+@patch('django.db.connection.cursor', side_effect=OperationalError())
+def test_exception_caught_when_connection_absent(mock_db_cursor):
+    """
+    When database connection is absent related errors are caught.
+    """
+    with pytest.raises(TimeoutError):
+        wait_for_database()
+
+    assert mock_db_cursor.called
 
 
 @patch('django.db.connection.cursor')
@@ -26,5 +32,7 @@ def test_loops_stable_times(mock_db_cursor):
     """
     Database connection must be stable 4 consecutive times in a row.
     """
-    call_command('wait_for_database')
-    assert mock_db_cursor.call_count == 4
+    with pytest.raises(TimeoutError):
+        wait_for_database()
+
+    assert mock_db_cursor.call_count == 3
