124e5b048c49f65effa7264a998d4c309f4094a7
Peter Bittner <peter.bittner@vshn.ch>
2020-03-04 13:10:59 +0100

    Release v1.3.0
    Fixes:
    - Compatibility with MySQL (and more database backends)
    
    Enhancements:
    - Use CommandError instead of SystemExit when timing out
    
    Thanks @jorrit-wehelp and @dismine!

--- django_probes/__init__.py
+++ django_probes/__init__.py
@@ -4,5 +4,5 @@ Make Django wait until database is ready. Probes for Docker and Kubernetes.
 __author__ = 'VSHN AG'
 __email__ = 'tech@vshn.ch'
 __url__ = 'https://github.com/vshn/django-probes'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __license__ = 'BSD-3-Clause'
