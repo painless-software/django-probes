b57d8fecceb5dc4f5b6586c00207709111c8a989
Peter Bittner <django@bittner.it>
2020-12-24 16:25:14 +0100

    Release v1.6.0
    Changes:
    - Migrate CI setup from Travis-CI to GitHub Actions
    - Support Python 3.9 + Django 3.1, drop Python 3.5
    - Move repository from @vshn to @painless-software
    - Use `from` when re-raising exceptions

--- django_probes/__init__.py
+++ django_probes/__init__.py
@@ -4,5 +4,5 @@ Make Django wait until database is ready. Probes for Docker and Kubernetes.
 __author__ = 'Peter Bittner'
 __email__ = 'django@bittner.it'
 __url__ = 'https://github.com/painless-software/django-probes'
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 __license__ = 'BSD-3-Clause'
