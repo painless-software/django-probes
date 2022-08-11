cec640e75552ff1f1b078d3f3a47700a9d0a8a38
Peter Bittner <django@bittner.it>
2022-08-11 22:21:30 +0200

    Release v1.7.0

--- django_probes/__init__.py
+++ django_probes/__init__.py
@@ -4,4 +4,4 @@ Make Django wait until database is ready. Probes for Docker and Kubernetes.
 __author__ = 'Peter Bittner'
 __email__ = 'django@bittner.it'
 __url__ = 'https://github.com/painless-software/django-probes'
-__version__ = '1.6.0'
+__version__ = '1.7.0'
