bb1588717c61fc213b08c0d5ae32af0c8336dba3
Peter Bittner <django@bittner.it>
2025-11-18 11:02:00 +0100

    Fix more linter complaints

--- tests/testproject/settings.py
+++ tests/testproject/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "tests" / "testproject.sqlite",
-    }
+    },
 }
 
 # Internationalization
