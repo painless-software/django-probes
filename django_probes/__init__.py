382e5c366b8a756f4151166c28e71d3de4356b80
Peter Bittner <django@bittner.it>
2025-11-18 08:41:43 +0100

    Modernize packaging and CI setup

--- django_probes/__init__.py
+++ django_probes/__init__.py
@@ -1,7 +1,5 @@
 """
 Make Django wait until database is ready. Probes for Docker and Kubernetes.
 """
-__author__ = 'Peter Bittner'
-__email__ = 'django@bittner.it'
-__url__ = 'https://github.com/painless-software/django-probes'
-__version__ = '1.7.0'
+
+__version__ = '1.8.0.dev0'
